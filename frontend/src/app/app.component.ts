9766fb7f45a3e9251bcbbdce7065afc2700ab240
funnytam1019 <tam.nguyen.10119@gmail.com>
2022-08-29 10:07:21 +0700

    minor config for demo

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { TreeGridComponent, VirtualScrollService } from '@syncfusion/ej2-angular-treegrid';
 import { Ajax } from '@syncfusion/ej2-base';
 import { EditSettingsModel } from '@syncfusion/ej2-angular-treegrid';
-import { SaveEventArgs } from '@syncfusion/ej2-grids';
 import { MenuEventArgs } from '@syncfusion/ej2-angular-navigations';
-import { Dialog } from '@syncfusion/ej2-angular-popups';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-container',
   templateUrl: './app.component.html',
@@ -19,7 +16,6 @@ export class AppComponent implements OnInit {
   public toolbar!: string[];
   public contextMenuItems!: Record<string, unknown>[];
   public taskData!: ITaskModel;
-  public modalService: NgbM
 
   @ViewChild('treegrid')
   public treeGridObj!: TreeGridComponent;
@@ -27,6 +23,12 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.toolbar = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
+    this.editSettings = {
+      allowAdding: true,
+      allowDeleting: true,
+      allowEditing: true,
+      mode: "Dialog"
+    }
     this.contextMenuItems = [
       { text: 'EditCol', target: '.e-headercontent', id: 'editcol' },
       { text: 'AddCol', target: '.e-headercontent', id: 'addcol' },
@@ -37,29 +39,38 @@ export class AppComponent implements OnInit {
       { text: 'FreezeCol', target: '.e-headercontent', id: 'freezecol' },
       { text: 'FilterCol', target: '.e-headercontent', id: 'filtercol' },
       { text: 'MultiSort', target: '.e-headercontent', id: 'multisort' },
+      { text: 'EditCol', target: '.e-content', id: 'editcol' },
+      { text: 'AddCol', target: '.e-content', id: 'addcol' },
+      { text: 'ViewCol', target: '.e-content', id: 'viewcol' },
+      { text: 'DelCol', target: '.e-content', id: 'delcol' },
+      { separator: true, target: '.e-content', id: 'separator' },
+      { text: 'ChooseCol', target: '.e-content', id: 'choosecol' },
+      { text: 'FreezeCol', target: '.e-content', id: 'freezecol' },
+      { text: 'FilterCol', target: '.e-content', id: 'filtercol' },
+      { text: 'MultiSort', target: '.e-content', id: 'multisort' },
     ]
   }
 
-  contextMenuClick(args: MenuEventArgs): void {
-    if (args.item.id === 'editcol') {
+  // contextMenuClick(args: MenuEventArgs): void {
+  //   if (args.item.id === 'editcol') {
 
-    }
-    else if (args.item.id === 'addcol') {
+  //   }
+  //   else if (args.item.id === 'addcol') {
 
-    } else if (args.item.id === 'viewcol') {
+  //   } else if (args.item.id === 'viewcol') {
 
-    } else if (args.item.id === 'delcol') {
+  //   } else if (args.item.id === 'delcol') {
 
-    } else if (args.item.id === 'choosecol') {
+  //   } else if (args.item.id === 'choosecol') {
 
-    } else if (args.item.id === 'freezecol') {
+  //   } else if (args.item.id === 'freezecol') {
 
-    } else if (args.item.id === 'filtercol') {
+  //   } else if (args.item.id === 'filtercol') {
 
-    } else if (args.item.id === 'multisort') {
+  //   } else if (args.item.id === 'multisort') {
 
-    }
-  }
+  //   }
+  // }
 
   click(): void {
     const ajax = new Ajax(
